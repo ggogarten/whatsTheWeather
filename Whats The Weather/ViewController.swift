9fa33af4419a28259de9612743ed78b335c5b6f6
George G <ggogarten@gmail.com>
2016-06-17 15:39:26 -0400

    scrapping weather info

--- Whats The Weather/ViewController.swift
+++ Whats The Weather/ViewController.swift
@@ -10,14 +10,59 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var cityTextField: UITextField!
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    @IBAction func findWeather(sender: AnyObject) {
+        
+        
+        
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let url = NSURL(string: "http://www.weather-forecast.com/locations/Miami/forecasts/latest")!
+        
+        let task = NSURLSession.sharedSession().dataTaskWithURL(url) { (data, response, error) in
+            
+            if let urlContent = data {
+                
+                let webContent = NSString(data: urlContent, encoding: NSUTF8StringEncoding)
+                
+                let websiteArray = webContent!.componentsSeparatedByString("3 Day Weather Forecast Summary:</b><span class=\"read-more-small\"><span class=\"read-more-content\"> <span class=\"phrase\">")
+              
+                if websiteArray.count > 0 {
+                    
+                 
+                    let weatherArray = websiteArray[1].componentsSeparatedByString("</span>")
+                    
+                    if weatherArray.count > 0 {
+                        
+                        let weatherSummary = weatherArray[0]
+                        
+                    }
+                    
+                   
+
+                }
+            }
+        }
+        
+        task.resume()
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        
+        
     }
 
 
