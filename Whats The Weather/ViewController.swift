c1c2cf10676aeafe007c8f8192bb7f41841446bc
George G <ggogarten@gmail.com>
2016-06-17 15:48:17 -0400

    working temperature pull

--- Whats The Weather/ViewController.swift
+++ Whats The Weather/ViewController.swift
@@ -17,6 +17,44 @@ class ViewController: UIViewController {
     
     @IBAction func findWeather(sender: AnyObject) {
         
+        let url = NSURL(string: "http://www.weather-forecast.com/locations/" + cityTextField.text!.stringByReplacingOccurrencesOfString(" ", withString: "-") + "/forecasts/latest")!
+        
+        let task = NSURLSession.sharedSession().dataTaskWithURL(url) { (data, response, error) in
+            
+            if let urlContent = data {
+                
+                let webContent = NSString(data: urlContent, encoding: NSUTF8StringEncoding)
+                
+                let websiteArray = webContent!.componentsSeparatedByString("3 Day Weather Forecast Summary:</b><span class=\"read-more-small\"><span class=\"read-more-content\"> <span class=\"phrase\">")
+                
+                if websiteArray.count > 1 {
+                    
+                    let weatherArray = websiteArray[1].componentsSeparatedByString("</span>")
+                    
+                    if weatherArray.count > 0 {
+                        
+                        let weatherSummary = weatherArray[0].stringByReplacingOccurrencesOfString("&deg;", withString: "º")
+                        
+                        dispatch_async(dispatch_get_main_queue(), {
+                            
+                            self.resultLabel.text = weatherSummary
+                            
+                            
+                        })
+                        
+                        
+                        
+                    }
+                    
+                    
+                    
+                }
+            }
+        }
+        
+        task.resume()
+
+        
         
         
     }
@@ -26,34 +64,6 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        let url = NSURL(string: "http://www.weather-forecast.com/locations/Miami/forecasts/latest")!
-        
-        let task = NSURLSession.sharedSession().dataTaskWithURL(url) { (data, response, error) in
-            
-            if let urlContent = data {
-                
-                let webContent = NSString(data: urlContent, encoding: NSUTF8StringEncoding)
-                
-                let websiteArray = webContent!.componentsSeparatedByString("3 Day Weather Forecast Summary:</b><span class=\"read-more-small\"><span class=\"read-more-content\"> <span class=\"phrase\">")
-              
-                if websiteArray.count > 0 {
-                    
-                 
-                    let weatherArray = websiteArray[1].componentsSeparatedByString("</span>")
-                    
-                    if weatherArray.count > 0 {
-                        
-                        let weatherSummary = weatherArray[0]
-                        
-                    }
-                    
-                   
-
-                }
-            }
-        }
-        
-        task.resume()
         
         
     }
